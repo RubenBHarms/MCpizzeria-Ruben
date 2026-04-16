2c4ade9d2b5faba06948cefda36b6692618e6879
RubenBHarms <rubenbharms@gamil.com>
2026-04-16 19:29:38 +0200

    alles klaar

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -1,9 +1,10 @@
 # Dit bestand zorgt voor de gebruikersinterface (GUI)van onze programma.
-# Vul hier de naam van je programma in:MC Pizzeria
+# Vul hier de naam van je programma in:
+# MCpizzaria
 #
 #
-# Vul hier jullie namen in:Ruben Harms
-#
+# Vul hier jullie namen in:
+# ruben harms
 #
 #
 
@@ -14,7 +15,49 @@ import MCPizzeriaSQL
 
 
 ### ---------  Functie definities  -----------------
+def zoekKlant():
+    #haal de ingevoerde_klantnaam op uit het invoerveld
+    # en gebruik dit om met SQL de klant in database te vinden
+    gevonden_klanten = MCPizzeriaSQL.zoekKlantInTabel(ingevoerde_klantnaam.get())
+    print(gevonden_klanten) # om te testen
+    
+    invoerveldKlantnaam.delete(0, END) #invoerveld voor naam leeg maken
+    invoerveldKlantNr.delete(0, END) #invoerveld voor klantNr leeg maken
+    for rij in gevonden_klanten: #voor elke rij dat de query oplevert
+        #toon klantnummer, de eerste kolom uit het resultaat in de invoerveld
+        invoerveldKlantNr.insert(END, rij[0])
+        #toon klantAchternaam, de tweede kolom uit het resultaat in de invoerveld
+        invoerveldKlantnaam.insert(END, rij[1])
 
+def toonMenuInListbox():
+    ListboxMenu.delete(0, END) #maak de listbox leeg
+    pizza_tabel = MCPizzeriaSQL.vraagOpGegevensPizzaTabel()
+    for regel in pizza_tabel:
+        ListboxMenu.insert(END, regel) #voeg elke regel uit resultaat in listboxMenu
+        ListboxMenu.insert(0, "ID Gerecht Prijs")
+
+def haalGeselecteerdeRijOp(event):
+    #bepaal op welke regel er geklikt is
+    geselecteerdeRegelInLijst = ListboxMenu.curselection()[0]
+    #haal tekst uit die regel
+    geselecteerdeTekst = ListboxMenu.get(geselecteerdeRegelInLijst)
+    #verwijder tekst uit veld waar je in wilt schrijven, voor het geval er al iets staat
+    invoerveldGeselecteerdePizza.delete(0, END)
+    #zet tekst in veld
+    invoerveldGeselecteerdePizza.insert(0, geselecteerdeTekst)
+
+#voeg de bestelling van klant met gekozen pizza en aantal toe
+#in de winkelwagentabel
+#en toon de bestelling in de listbox op het scherm
+def voegToeAanWinkelWagen():
+    klantNr = invoerveldKlantNr.get()
+    gerechtID = invoerveldGeselecteerdePizza.get()
+    aantal = optionMenuPizzaAantal.cget()
+    MCPizzeriaSQL.voegToeAanWinkelWagen(klantNr, gerechtID, aantal )
+    winkelwagen_tabel = MCPizzeriaSQL.vraagOpGegevensWinkelWagenTabel()
+    ListboxWinkelwagen.delete(0, END) #listbox eerst even leeg maken
+    for regel in winkelwagen_tabel:
+        ListboxWinkelwagen.insert(END, regel)
 
 ### --------- Hoofdprogramma  ---------------
 
@@ -22,24 +65,45 @@ venster = Tk()
 venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
 venster.wm_title("MC Pizzeria")
 
-knopSluit = Button(venster, text="Sluiten", width=12, command=venster.destroy)
-knopSluit.grid(row=0, column=0)
-
 labelIntro = Label(venster, text="Welkom!")
 labelIntro.grid(row=0, column=0, sticky="W")
-
 knopSluit = Button(venster, text="Sluiten",width=12,command=venster.destroy)
 knopSluit.grid(row=17, column=4)
 
-klantnaam = Label(venster, text="Klantnaam")
-klantnaam.grid(row=1, column=0, sticky="W")
+labelPizza = Label(venster, text="Gekozen pizza")
+labelPizza.grid(row=8, column=0, sticky="W")
+invoerveldGeselecteerdePizza = Entry(venster)
+invoerveldGeselecteerdePizza.grid(row=8, column=1, sticky="W")
 
+labelKlantnaam = Label(venster, text="Klantnaam!")
+labelKlantnaam.grid(row=1, column=0, sticky="W")
 ingevoerde_klantnaam = StringVar()
 invoerveldKlantnaam = Entry(venster, textvariable=ingevoerde_klantnaam)
 invoerveldKlantnaam.grid(row=1, column=1, sticky="W")
-
 invoerveldKlantNr = Entry(venster)
 invoerveldKlantNr.grid(row=2, column=1, sticky="W")
+
+knopZoekOpKlantnaam = Button(venster, text="Zoek klant", width=12, command=zoekKlant)
+knopZoekOpKlantnaam.grid(row=1, column=4)
+
+ListboxMenu = Listbox(venster, height=6, width=50)
+ListboxMenu.grid(row=2, column=1, rowspan=6, columnspan=2, sticky="W")
+ListboxMenu.bind('<<ListboxSelect>>', haalGeselecteerdeRijOp)
+
+knopToonPizzas = Button(venster, text="Toon alle pizza’s", width=12, command=toonMenuInListbox)
+knopToonPizzas.grid(row=3, column=4)
+
+aantalGekozen = IntVar()
+aantalGekozen.set(1)
+optionMenuPizzaAantal = OptionMenu(venster, aantalGekozen, 1,2,3)
+optionMenuPizzaAantal.grid(row=9, column=1, sticky="W")
+
+VoegToeWinkelwagen = Button(venster, text="Voeg toe aan winkelwagen", command=voegToeAanWinkelWagen)
+VoegToeWinkelwagen.grid(row=11, column=0, sticky="W")
+
+ListboxWinkelwagen = Listbox(venster, height=6, width=50)
+ListboxWinkelwagen.grid(row=12, column=1, rowspan=6, columnspan=2, sticky="W")
+ListboxWinkelwagen.bind("<<ListboxSelect>>", voegToeAanWinkelWagen)
 
 
 
--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -1,8 +1,8 @@
-# Vul hier de naam van je programma in:MC Pizzeria
+# Vul hier de naam van je programma in:
+# MCpizzaria
 #
-#
-# Vul hier jullie namen in:Ruben Harms
-#
+# Vul hier jullie namen in:
+# ruben harms
 #
 #
 
@@ -14,36 +14,50 @@ with sqlite3.connect("MCPizzeria.db") as db:
     cursor = db.cursor()#cursor is object waarmee je data uit de database kan halen
 
 
-### ---------  Functie definities  ---
+### ---------  Functie definities  -----------------
 def maakTabellenAan():
- # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
- cursor.execute("""
- CREATE TABLE IF NOT EXISTS tbl_pizzas(
- gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
- gerechtNaam TEXT NOT NULL,
- gerechtPrijs REAL NOT NULL);""")
- print("Tabel 'tbl_pizzas")
- 
+    # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tbl_pizzas(
+        gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+        gerechtNaam TEXT NOT NULL,
+        gerechtPrijs REAL NOT NULL);""")
+    print("Tabel 'tbl_pizzas' aangemaakt.")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tbl_klanten(
+        klantNr INTEGER PRIMARY KEY AUTOINCREMENT,
+        klantAchternaam TEXT);""")
+    print("Tabel 'tbl_klanten' aangemaakt.")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tbl_winkelWagen(
+    bestelRegel INTEGER PRIMARY KEY AUTOINCREMENT,
+    klantNr INTEGER,
+    gerechtID INTEGER,
+    aantal INTEGER NOT NULL,
+    FOREIGN KEY (klantNr) REFERENCES tbl_klanten(klantNr)
+    FOREIGN KEY (gerechtID) REFERENCES tbl_pizzas(gerechtID));""")
+    print("Tabel 'tbl_winkelWagen' aangemaakt.")
+
+
 def printTabel(tabel_naam):
- cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen
- opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele
- print("Tabel " + tabel_naam + ":", opgehaalde_gegevens)
- 
+    cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen
+    opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele
+    print("Tabel " + tabel_naam + ":", opgehaalde_gegevens) #druk gegevens af
 
 def voegPizzaToe(naam_nieuwe_pizza, prijs_nieuwe_pizza):
- cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ? )", (naam_nieuwe_pizza, prijs_nieuwe_pizza))
- db.commit() #gegevens naar de database wegschrijven
- print("Pizza toegevoegd:")
- printTabel("tbl_pizzas")
+    cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ? )", (naam_nieuwe_pizza, prijs_nieuwe_pizza))
+    db.commit() #gegevens naar de database wegschrijven
+    print("Pizza toegevoegd:")
+    printTabel("tbl_pizzas")
 
 def verwijderPizza(gerechtNaam):
- cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,))
- print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam )
- db.commit() #gegevens naar de database wegschrijven
- printTabel("tbl_pizzas")
+    cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,))
+    print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam )
+    db.commit() #gegevens naar de database wegschrijven
+    printTabel("tbl_pizzas")
 
 def pasGerechtAan(gerechtID, nieuweGerechtNaam, nieuwePrijs):
-    cursor.execute("UPDATE tbl_pizzas SET gerechtNaam = ?,gerechtPrijs = ? WHERE gerechtID = ?", (nieuweGerechtNaam,nieuwePrijs, gerechtID ))
+    cursor.execute("UPDATE tbl_pizzas SET gerechtNaam = ?, gerechtPrijs = ? WHERE gerechtID = ?", (nieuweGerechtNaam, nieuwePrijs, gerechtID ))
     db.commit() #gegevens naar de database wegschrijven
     print("Gerecht aangepast")
     printTabel("tbl_pizzas")
@@ -54,10 +68,53 @@ def voegKlantToe(naam_nieuwe_klant):
     print("Klant toegevoegd:")
     printTabel("tbl_klanten")
 
+#Zoek alle gegevens over klant met ingevoerde naam
+def zoekKlantInTabel(ingevoerde_klantnaam):
+    cursor.execute("SELECT * FROM tbl_klanten WHERE klantAchternaam = ?", (ingevoerde_klantnaam,))
+    zoek_resultaat = cursor.fetchall()
+    if zoek_resultaat == []: #resultaat is leeg, geen gerecht gevonden
+        print("Geen klant gevonden met achternaam", ingevoerde_klantnaam)
+        print("Klant wordt nu toegevoegd.")
+        cursor.execute("INSERT INTO tbl_klanten VALUES(NULL, ? )", (ingevoerde_klantnaam, ))
+        db.commit() #gegevens in de database zetten
+        print("Klant toegevoegd aan 'tbl_klanten':" + ingevoerde_klantnaam )
+        printTabel("tbl_klanten")
+        #nu dat klant in tabel is gezet, kunnen we zijn gegevens ophalen
+        cursor.execute("SELECT * FROM tbl_klanten WHERE klantAchternaam = ?",(ingevoerde_klantnaam,))
+        zoek_resultaat = cursor.fetchall()
+
+    
+    
+    return zoek_resultaat
+
+def vraagOpGegevensPizzaTabel():
+    cursor.execute("SELECT * FROM tbl_pizzas")
+    resultaat = cursor.fetchall()
+    print("Tabel tbl_pizzas:", resultaat)
+    return resultaat
+
+def voegToeAanWinkelWagen(klantNr, gerechtID, aantal):
+    cursor.execute("INSERT INTO tbl_winkelWagen VALUES(NULL, ?, ?, ?)", (klantNr, gerechtID, aantal,))
+    db.commit()#gegevens in de database zetten
+    printTabel("tbl_winkelWagen")
+
+def vraagOpGegevensWinkelWagenTabel():
+    cursor.execute("SELECT * FROM tbl_winkelWagen")
+    resultaat = cursor.fetchall()
+    print("Tabel tbl_winkelWagen:", resultaat)
+    return resultaat
+
+
 ### --------- Hoofdprogramma  ---------------
-maakTabellenAan()
-printTabel("tbl_pizzas")
+#maakTabellenAan()
+#printTabel("tbl_pizzas")
 #voegPizzaToe("Margarita", 9.50)
-#voegPizzaToe("Hawai", 12.50)
-#voegPizzaToe("Salami", 10.00)
-#verwijderPizza("Hawai")
+#voegPizzaToe("Hawaii", 12.25)
+#voegPizzaToe("Salami", 10.0)
+#verwijderPizza("Hawaii")
+#pasGerechtAan(3, "Salamiii", 19.25)
+#voegKlantToe("Janssen")
+
+
+
+
